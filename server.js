bdf91db09ca093b1565054aabb0f3af65f3b57d3
kd <kd@Karans-MacBook-Pro.local>
2022-01-23 01:48:18 +0530

    Deployment Changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const port = 3003;
+const port = process.env.PORT || 3003;
 const publicPath = path.join(__dirname, "public", "build");
 const expressStaticGzip = require("express-static-gzip");
 const app = express();
